3041e14d572454d9bb6da34a4d4a1f48f6398989
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-04 15:37:46 +0530

    Admin Panel set Polygons

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 8000;
 const path = require("path");
 
-const staticRoutes = require("./routes/static.js");
+const staticRoutes = require("./routes/static");
+const adminRoutes = require("./routes/admin");
 
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", staticRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
