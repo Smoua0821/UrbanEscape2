0f6d0a7055e629f93d9360dd13aa343cb1c3039a
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-05 08:51:12 +0530

    Admin Panel set image and radius

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
 async function adminPage(req, res) {
+  const dirPath = "./public/images/mapicons/";
+  const icons = fs
+    .readdirSync(dirPath)
+    .filter((file) => fs.statSync(path.join(dirPath, file)).isFile())
+    .map((file) => path.join(file));
+
+  console.log(icons);
   res.render("pages/admin", {
     apiKey: "AIzaSyBaQ334LSpDNZXU8flkT1VjGpdj7f3_BZI",
+    icons: icons,
   });
 }
 module.exports = { adminPage };
