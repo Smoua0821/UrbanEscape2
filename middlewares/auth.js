17b54061951ca0e7d01dc550f644529000c3e128
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-06 21:34:01 +0530

    All User can access Homepage

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,10 @@
 const jwt = require("jsonwebtoken");
+const publicRoutes = ["/home", "/"];
+
 const authMiddleware = (req, res, next) => {
+  if (publicRoutes.includes(req.path)) {
+    return next();
+  }
   const token = req.cookies.sessionId;
 
   if (!token) {
