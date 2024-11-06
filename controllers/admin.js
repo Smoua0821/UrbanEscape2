1504032176436de836dc231e932c4d47658ceddc
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-06 21:53:58 +0530

    Showing Users in Admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const User = require("../models/User");
 async function adminPage(req, res) {
   const user = req.user;
   if (user.role.current != "admin") return res.end("Unauthorised!");
@@ -11,8 +11,10 @@ async function adminPage(req, res) {
     .map((file) => path.join(file));
 
   console.log(icons);
+  const users = await User.find();
   res.render("pages/admin", {
     apiKey: "AIzaSyBaQ334LSpDNZXU8flkT1VjGpdj7f3_BZI",
+    users: users,
     icons: icons,
   });
 }
