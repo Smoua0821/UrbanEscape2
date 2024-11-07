a994e285620f76854fa05556e682019feb5884d0
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-07 09:26:04 +0530

    Interlinking Pages

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 const userProfile = async (req, res) => {
   const user = req.user;
-  return res.status(200).json(user);
+  //   return res.status(200).json(user)
+  return res.render("pages/profile", {
+    user: user,
+  });
 };
 
 module.exports = { userProfile };
