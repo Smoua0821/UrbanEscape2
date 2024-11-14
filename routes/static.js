d02d961340ced8682d7ae6496af88544bd48961a
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-14 20:30:12 +0530

    Showing Map by Link for USER

--- routes/static.js
+++ routes/static.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Map = require("../models/Map");
+
 router.get("/", (req, res) => {
+  return res.send("Homepage");
+});
+router.get("/:mapId", async (req, res) => {
+  const { mapId } = req.params;
+  if (!mapId) return res.status(301).redirect("/");
+  const map = await Map.findOne({ id: mapId });
+  if (!map)
+    return res.render("pages/notifier", {
+      type: "error",
+      message: "No Map Found",
+    });
   const user = req.user;
   res.render("pages/home", {
     apiKey: "AIzaSyBaQ334LSpDNZXU8flkT1VjGpdj7f3_BZI",
     user: user,
+    mapParsed: mapId,
   });
 });
 
