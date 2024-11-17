f59175cd57e9ca78b4d29605cfcade9130d4033f
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-17 18:02:17 +0530

    Showing Captured Images to User

--- routes/static.js
+++ routes/static.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Map = require("../models/Map");
 const User = require("../models/User");
-
+const jwt = require("jsonwebtoken");
 router.get("/", (req, res) => {
   return res.render("pages/landing", {
     user: req.user,
@@ -18,12 +18,33 @@ router.get("/map/:mapId", async (req, res) => {
       message: "Map Not found!",
     });
   const user = req.user;
+  let imgexist = [];
+  const token = req.cookies.sessionId;
+  if (token) {
+    try {
+      const userO = await jwt.verify(token, process.env.JWT_SECRET);
+      if (userO) {
+        const user = await User.findOne({ email: userO.email });
+        if (user) {
+          imgexist = user.capturedImages.find(
+            (ci) => ci.mapId.toString() === map._id.toString()
+          );
+          if (!imgexist) imgexist = [];
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (!imgexist) imgexist = [];
   res.render("pages/home", {
     apiKey: "AIzaSyBaQ334LSpDNZXU8flkT1VjGpdj7f3_BZI",
     user: user,
     mapParsed: mapId,
     title: map.name,
     missions: map.missions,
+    imageExist: imgexist,
   });
 });
 
