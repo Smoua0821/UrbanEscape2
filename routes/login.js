a556b3b19d4c71f1e8add0f7962b52e5352ac5ce
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-28 20:06:26 +0530

    Countries name from DB

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,9 @@ router.get("/", loginPage);
 router.post("/", loginValidate);
 
 router.post("/new", newUser);
+
+router.get("/countries", (req, res) => {
+  const countries = ["Alberta", "Monnesta"];
+  return res.json({ status: "success", countries: countries });
+});
 module.exports = router;
