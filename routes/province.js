c62bdfc401c4a2820d3960bf84c1690822e7e1b9
Vijay Singh <thakurvijayofficial@gmail.com>
2024-11-30 07:12:18 +0530

    Crud on Province

--- routes/province.js
+++ routes/province.js
@@ -32,7 +32,9 @@ router.delete("/:id", async (req, res) => {
       return res.status(404).json({ error: "Province not found" });
     }
     await province.deleteOne();
-    res.status(200).json({ message: "Province deleted successfully" });
+    res
+      .status(200)
+      .json({ message: `Province ${province.name} deleted successfully` });
   } catch (err) {
     res
       .status(500)
