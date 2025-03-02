e02a2561848ad475ec73313f108fcf6860bc7e86
Vijay Singh <thakurvijayofficial@gmail.com>
2025-03-02 19:26:49 +0530

    Post Register message page updation

--- routes/login.js
+++ routes/login.js
@@ -17,6 +17,16 @@ router.post("/", loginValidate);
 
 router.get("/verify/:codeId", verifyActivationEmail);
 
+router.get("/password/recovery", (req, res) => {
+  return res.render("pages/postRegister", {
+    title: "Recover password",
+    icon: "key",
+    message:
+      "You will Recover your password from here, currently this feature is in maintenance mode!",
+    type: "success",
+  });
+});
+
 router.post("/signup", newUser);
 
 router.get("/countries", provinceList);
