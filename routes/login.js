2acaebab88d228e020894daf964e414ffdd6d418
Vijay Singh <thakurvijayofficial@gmail.com>
2025-03-02 20:17:59 +0530

    Password Recovery page

--- routes/login.js
+++ routes/login.js
@@ -18,12 +18,10 @@ router.post("/", loginValidate);
 router.get("/verify/:codeId", verifyActivationEmail);
 
 router.get("/password/recovery", (req, res) => {
-  return res.render("pages/postRegister", {
-    title: "Recover password",
-    icon: "key",
-    message:
-      "You will Recover your password from here, currently this feature is in maintenance mode!",
-    type: "success",
+  return res.render("pages/passwordRecovery", {
+    title: "Set Password",
+    type: "setpassword",
+    CAPTCHA_KEY: process.env.CAPTCHA_SITE_KEY,
   });
 });
 
