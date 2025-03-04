fecabce03ebbd1983cea4b5a93e93e0fa166c1a2
Vijay Singh <thakurvijayofficial@gmail.com>
2025-03-05 01:27:04 +0530

    Rate limit in Login

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   loginPage,
   loginValidate,
@@ -14,9 +15,18 @@ const {
   setPasswordRecovery,
 } = require("../controllers/login");
 
+const rateLimit = require("express-rate-limit");
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Allow only 5 login attempts per 15 minutes
+  message: "Too many login attempts. Try again after 15 minutes.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 router.get("/", loginPage);
 
-router.post("/", loginValidate);
+router.post("/", loginLimiter, loginValidate);
 
 router.get("/verify/:codeId", verifyActivationEmail);
 
@@ -28,15 +38,23 @@ router.get("/password/recovery", (req, res) => {
   });
 });
 
-router.post("/password/recovery/request", requestPasswordRecovery);
+router.post(
+  "/password/recovery/request",
+  loginLimiter,
+  requestPasswordRecovery
+);
 router.get("/password/recovery/verify/:codeId", handlePasswordRecovery);
-router.post("/password/recovery/verify/:codeId", setPasswordRecovery);
+router.post(
+  "/password/recovery/verify/:codeId",
+  loginLimiter,
+  setPasswordRecovery
+);
 
-router.post("/signup", newUser);
+router.post("/signup", loginLimiter, newUser);
 
 router.get("/countries", provinceList);
 
-router.post("/plugins", pluginLoginController);
+router.post("/plugins", loginLimiter, pluginLoginController);
 router.get("/verifyPlugin", setPluginLogin);
 
 router.get("/github/callback", gitOAuthVerify);
